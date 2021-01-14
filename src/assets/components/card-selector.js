b41f3729364f9e816e81e1a63ce8d10c6e163cf4
Eivind Omtveit <e.omtveit@gmail.com>
2021-01-14 13:31:56 +0100

    removed Mark tel number, legal reasons

--- src/assets/components/card-selector.js
+++ src/assets/components/card-selector.js
@@ -60,8 +60,9 @@ class CardSelector extends Component {
                         <Employee active={ this.state.selected === 3 ? true : false } number={3} img={tm3} fName="Christian" lName="Larssen" position="Head of Project Management" phone="+47 92817937" email="cel@nordicflow.no" description={descriptionArray[2]} experience={experienceArray[2]} handleEmployeeClick={this.handleEmployeeClick}/>
                     </li>
 
+                    {/* Mark tlf: +47 46943819 */}
                     <li>
-                        <Employee active={ this.state.selected === 4 ? true : false } number={4} img={tm4} fName="Mark" lName="Tuinman" position="Head of Sales & Marketing" phone="+47 46943819" email="mt@nordicflow.no" description={descriptionArray[3]} experience={experienceArray[3]} handleEmployeeClick={this.handleEmployeeClick}/>
+                        <Employee active={ this.state.selected === 4 ? true : false } number={4} img={tm4} fName="Mark" lName="Tuinman" position="Head of Sales & Marketing" phone="tel pending" email="mt@nordicflow.no" description={descriptionArray[3]} experience={experienceArray[3]} handleEmployeeClick={this.handleEmployeeClick}/>
                     </li>
 
                     <li>
@@ -81,7 +82,8 @@ class CardSelector extends Component {
 
                     <Employee active={ this.state.selected === 3 ? true : false }  number={3} img={tm3} fName="Christian" lName="Larssen" position="Head of Project Management" phone="+47 92817937" email="cel@nordicflow.no" description={descriptionArray[2]} experience={experienceArray[2]} handleEmployeeClick={this.handleEmployeeClick}/>
 
-                    <Employee active={ this.state.selected === 4 ? true : false } number={4} img={tm4} fName="Mark" lName="Tuinman" position="Head of Sales & Marketing" phone="+47 46943819" email="mt@nordicflow.no" description={descriptionArray[3]} experience={experienceArray[3]} handleEmployeeClick={this.handleEmployeeClick}/>
+                    {/* Mark tlf: +47 46943819 */}
+                    <Employee active={ this.state.selected === 4 ? true : false } number={4} img={tm4} fName="Mark" lName="Tuinman" position="Head of Sales & Marketing" phone="tel pending" email="mt@nordicflow.no" description={descriptionArray[3]} experience={experienceArray[3]} handleEmployeeClick={this.handleEmployeeClick}/>
 
                     <Employee active={ this.state.selected === 5 ? true : false }  number={5} img={tm5} fName="Per Einar" lName="Wethe" position="Chief Executive Officer" phone="+47 45246880" email="pew@nordicflow.no" description={descriptionArray[4]} experience={experienceArray[4]} handleEmployeeClick={this.handleEmployeeClick}/>
 
