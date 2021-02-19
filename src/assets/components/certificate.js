e3f5f566df536139eb7cfa16297f9595b073ae24
Eivind Omtveit <e.omtveit@gmail.com>
2021-02-19 14:30:00 +0100

    certificate animation working for desktop

--- src/assets/components/certificate.js
+++ src/assets/components/certificate.js
@@ -1,9 +1,24 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 
 class Certificate extends Component {
 
+
     render() {
+
+
+        let showCertificate = () => {
+            let pdf = document.getElementById(this.props.name + "-pdf")
+            let bg = document.querySelector(".cert-pdf-bg")
+            let top = window.scrollY + 106
+
+            // window.scrollTo(0,0)
+            bg.classList.add("active-pdf-bg")
+            setTimeout(function() {
+                pdf.style.top = top.toString() + "px"
+                pdf.classList.add("active-pdf")
+            }, 300)
+        }
 
         let logo = []
 
@@ -29,8 +44,12 @@ class Certificate extends Component {
             <div className="certificate">
                 {logo}
                 <h3>{this.props.name}</h3>
-                {/* <h4 className="cert-view">view certificate</h4> */}
+                <h4 className="cert-view" onMouseDown={() => showCertificate()}>view certificate</h4>
                 <p>{this.props.cText}</p>
+
+                <div className="pdf-container" id={this.props.name + "-pdf"} ref={this.wrapperRef}>
+                    <img src={this.props.pdf} alt="" className="pdf"/>
+                </div>
             </div>
         )
     }
--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -1,14 +1,31 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Certificate from '../assets/components/certificate'
 import achillesImg from '../assets/certificate-logo/achilles.png'
 import kiwaImg from '../assets/certificate-logo/kiwa.png'
 import epimImg from '../assets/certificate-logo/epim.png'
 import norAccredImg from '../assets/certificate-logo/nor-accred.png'
-import isoImg from '../assets/certificates/ISO 9001_certificate no. 2178.png'
+import kiwaCert from '../assets/certificates/ISO 9001_certificate no. 2178.png'
+import epimCert from '../assets/certificates/EPIM-certificate.png'
+import achillesCert from '../assets/certificates/Achilles-certificate.png'
 
 
 class Certifications extends Component {
+
     render() {
+
+        let handleBgClick = () => {
+            let pdf = document.querySelector(".active-pdf")
+            let bg = document.querySelector(".active-pdf-bg")
+
+            if (document.querySelector(".active-pdf-bg") && document.querySelector(".active-pdf")){
+                
+                pdf.classList.remove("active-pdf")
+                setTimeout(function() {
+                    bg.classList.remove("active-pdf-bg")
+                }, 500)
+            }
+        }
+
         return(
             <div className="cert-container">
                 <div className="cert-title">
@@ -21,15 +38,13 @@ class Certifications extends Component {
                     <h3>OUR <span>CERTIFICATIONS</span></h3>
                 </div>
                 <div className="certificates-container">
-                    <Certificate name="Achilles JQS" logo={achillesImg} cText="Nordic Flow AS is qualified in the Achilles Joint Qualification System for suppliers to the Oil Industry in Norway and Denmark."/>
-                    <Certificate name="KIWA" logo={kiwaImg} logo2={norAccredImg} cText="Nordic Flow AS` Quality Management System, QMS is certified to ISO 9001:2015. Our QMS is the foundation of quality assurance activities to improve the overall performance and provide a sound basis for sustainable development activities."/>
-                    <Certificate name="EPIM JQS" logo={epimImg} cText="Nordic Flow AS is registered in EPIM JQS as a supplier. EPIM JQS is a joint qualification system used by all of the operators on the Norwegian Continental Shelf."/>
+                    <Certificate pdf={kiwaCert} name="KIWA" logo={kiwaImg} logo2={norAccredImg} cText="Nordic Flow AS` Quality Management System, QMS is certified to ISO 9001:2015. Our QMS is the foundation of quality assurance activities to improve the overall performance and provide a sound basis for sustainable development activities."/>
+                    <Certificate pdf={epimCert} name="EPIM JQS" logo={epimImg} cText="Nordic Flow AS is registered in EPIM JQS as a supplier. EPIM JQS is a joint qualification system used by all of the operators on the Norwegian Continental Shelf."/>
+                    <Certificate pdf={achillesCert} name="Achilles JQS" logo={achillesImg} cText="Nordic Flow AS is qualified in the Achilles Joint Qualification System for suppliers to the Oil Industry in Norway and Denmark."/>
                 </div>
 
-                {/* <div className="cert-pdf-bg"></div> */}
-                {/* <div className="cert-pdf-placeholder">
-                    <img src={isoImg} alt="" className="pdf"/>
-                </div> */}
+                <div className="cert-pdf-bg" onMouseDown={() => handleBgClick()}></div>
+                
             </div>
         )
     }
